e836d348b944a58c070ba2bc45dc17a54daf27da
Oras Al-Kubaisi <code@oras.me>
2024-07-27 22:37:17 +0100

    added the templates folder to the build

--- setup.py
+++ setup.py
@@ -14,8 +14,11 @@ requirements_path = os.path.join(os.path.dirname(__file__), "requirements.txt")
 
 setup(
     name="thabit",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
+    package_data={
+        "thabit": ["templates/*"],
+    },
     entry_points={
         "console_scripts": [
             "thabit=thabit.thabit:cli",
