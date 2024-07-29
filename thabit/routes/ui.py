b3ee1fc8591990ff151f2c4a1caf0109e0878c1c
Oras Al-Kubaisi <code@oras.me>
2024-07-29 09:43:06 +0100

    create a dataset folder

--- thabit/routes/ui.py
+++ thabit/routes/ui.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template, request, jsonify
 import json
 import os
 from thabit.utils.logger import get_logger
+from thabit.constants.platform import DATASET_FOLDER
+from glob import glob
 
 app = Flask(__name__)
-logger = get_logger()
+# logger = get_logger(function_name="ui.py")
 
 # set the template folder to the root folder
 app.template_folder = os.path.join(os.path.dirname(__file__), "..", "templates")
-logger.info(f"Template folder: {app.template_folder}")
+# logger.info(f"Template folder: {app.template_folder}")
 
 
 @app.route("/config", methods=["GET"])
@@ -25,8 +27,40 @@ def config():
 
 
 @app.route("/dataset", methods=["GET"])
-def dataset(dataset_path):
-    return render_template("dataset.html")
+def dataset():
+    dataset_path = request.args.get("dataset")
+    version = request.args.get("version")
+    full_dataset_path = os.path.join(DATASET_FOLDER, dataset_path)
+    if not version:
+        version = 1
+    # check if the file exists
+    if not os.path.exists(full_dataset_path):
+        dataset = {}
+        # create the folder
+        os.makedirs(full_dataset_path)
+    else:
+        # get the dataset latest version
+        # check if the folder has any .json files
+        if glob(full_dataset_path + "/*.json"):
+            latest_version = max(
+                [
+                    int(f.split(".")[0])
+                    for f in os.listdir(full_dataset_path)
+                    if f.endswith(".json")
+                ]
+            )
+        else:
+            latest_version = 0
+
+        if latest_version > 0:
+            with open(
+                full_dataset_path + f"/{latest_version}.json",
+                "r",
+            ) as f:
+                dataset = json.load(f)
+        else:
+            dataset = {}
+    return render_template("dataset.html", dataset=dataset)
 
 
 @app.route("/save_config", methods=["POST"])
@@ -42,10 +76,29 @@ def save_config(config_path):
 
 @app.route("/save_dataset", methods=["POST"])
 def save_dataset(dataset_path):
-    # check if the path exists
-    if not os.path.exists(dataset_path):
-        os.makedirs(dataset_path)
+    logger = get_logger(function_name="save_dataset")
+    full_dataset_path = os.path.join(DATASET_FOLDER, dataset_path)
+    # the files in the folder are named as 1.json, 2.json, 3.json etc. referring to the version.
+    # check the latest version and add a new one
+    try:
+        latest_version = max(
+            [
+                int(f.split(".")[0])
+                for f in os.listdir(full_dataset_path)
+                if f.endswith(".json")
+            ]
+        )
+        if latest_version == "":
+            latest_version = 0
+        new_version = latest_version + 1
+    except Exception as e:
+        logger.error(f"Error getting latest version: {e}")
+        return jsonify({"status": "error", "message": "Error getting latest version"})
     # save the dataset file
-    with open(dataset_path, "w") as f:
-        json.dump(request.json, f)
+    try:
+        with open(full_dataset_path + f"/{new_version}.json", "w") as f:
+            json.dump(request.json, f, indent=4)
+    except Exception as e:
+        logger.error(f"Error saving dataset: {e}")
+        return jsonify({"status": "error", "message": "Error saving dataset"})
     return jsonify({"status": "success"})
